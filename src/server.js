7cd82637a16507e3328c8675d2e09d08d0fcb968
Vagner Bruno Batista <85170863+vagnerbruno85@users.noreply.github.com>
2022-09-05 19:32:44 -0300

    feat: Criando arquivo de rotas - Task 20

--- src/server.js
+++ src/server.js
@@ -2,9 +2,20 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = process.env.APP_PORT || 3333;
+const http = require('http');
 
 app.use(express.json())
 
+const server = http.createServer(app);
+const router = express.Router();
+
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
+//Carrega as Rotas
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
 app.listen(port, function () {
     console.log(`app listening on port ${port}`)
-})
+});
