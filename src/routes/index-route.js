d745330a7f1f1b2fcfbdb1c1b9055e990531dc91
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2022-09-21 22:32:33 -0300

    feat: Criando Front-End com EJS - task-12

--- src/routes/index-route.js
+++ src/routes/index-route.js
@@ -1,8 +1,77 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
-router.get('/', (req,res,next) => {
-    res.status(200).send({title:"Node Store API", version: "0.0.2"})
+router.get('/', async (req,res) => {
+    let produtos;
+
+    try {        
+
+        const response = await axios.get('http://localhost:3333/produto?size=100&pagina=1&order=desc&col=valor');
+        console.log("DATA: ",response.data);
+        produtos = response.data;
+    } catch (error) {
+        console.log("erro",error);
+        produtos = [
+            {nome: "Tenis preto", modelo: "nike", valor: 378},
+            {nome: "Tenis branco", modelo: "nike", valor: 323},
+            {nome: "Sapatenis", modelo: "adida", valor: 123},
+            {nome: "olimpicos", modelo: "modelo 22", valor: 170},
+            {nome: "Tenis", modelo: "nike", valor: 69}
+        ];
+    }
+
+    res.render('../views/home', {
+        products: produtos
+    });
+
 });
+
+// router.get('/clientes-pagina', async (req,res) => {
+//     let clientes;
+
+//     try {        
+
+//         const response = await axios.get('http://localhost:3333/clientes?');
+//         console.log("DATA: ",response.data);
+//         clientes = response.data;
+//     } catch (error) {
+//         console.log("erro",error);
+//         clientes = [
+//             {nome: "Carla Ferreira", email: "carlinha@gmail.com", cpf: 15825865799},
+//             {nome: "Lucas Alves", email: "lulu.alves@gmail.com", cpf: 76525865799},
+//             {nome: "Luana Mello Ferreira", email: "mel@hotmail.com", cpf: 15833365799},
+//             {nome: "Gabriel Pereira", email: "biel.pepe@bol.com", cpf: 15221865799}
+//         ];
+//     }
+
+//     res.render('../views/home', {
+//         clients: clientes
+//     });
+
+// });
+
+// router.get('/pedidos-pagina', async (req,res) => {
+//     let pedidos;
+
+//     try {        
+
+//         const response = await axios.get('http://localhost:3333/pedidos?');
+//         console.log("DATA: ",response.data);
+//         pedidos = response.data;
+//     } catch (error) {
+//         console.log("erro",error);
+//         pedidos = [
+//             {valor_total: 348, descricao: "Tênis Nike Revolution 6 Next Nature Masculino - Preto+Branco", status_geral: 1},
+//             {valor_total: 547, descricao: "Tênis Asics Gel-Impression 10 Feminino - Preto", status_geral: 0},
+//             {valor_total: 1200, descricao: "Chuteira Society Penalty Rx Locker XXI Penalty - Laranja + Chuteira Penalty RX Locker XXI Society Amarela - Amarelo", status_geral: 1}
+//         ];
+//     }
+
+//     res.render('../views/home', {
+//         requests: pedidos
+//     });
+
+// });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.APP_PORT || 3333;
 const http = require('http');
+const axios = require('axios');
 app.use(express.static('public'));
 
 
@@ -32,32 +33,12 @@ const clienteRoute = require('./routes/cliente-route');
 
 
 //Carrega as Rotas
-//app.use('/', indexRoute);
+app.use('/', indexRoute);
 app.use('/produto', produtoRoute);
 app.use('/marcas', marcaRoute);
 app.use("/contato", contatoRoute);
 app.use('/endereco', enderecoRoute);
 app.use('/cliente', clienteRoute);
-
-
-
-app.get('/', (req,res) => {
-    var produtos = [
-        {nome: "Tenis preto", modelo: "nike", valor: 378},
-        {nome: "Tenis branco", modelo: "nike", valor: 323},
-        {nome: "Sapatenis", modelo: "adida", valor: 123},
-        {nome: "olimpicos", modelo: "modelo 22", valor: 170},
-        {nome: "Tenis", modelo: "nike", valor: 69}
-    ]
-
-    // let produtos  = app.get('/produto',(req,res) => {
-    //     req.
-    // });
-
-    res.render('../views/home', {
-        products: produtos
-    });
-});
 
 app.get('/sobre', (req,res) => {
     res.render('../views/about');
@@ -71,7 +52,7 @@ app.get('/cadastro', (req,res) => {
     res.render('../views/client-register');
 });
 
-app.get('/contato', (req,res) => {
+app.get('/contato-pagina', (req,res) => {
     res.render('../views/contact');
 });
 
@@ -79,7 +60,7 @@ app.get('/pagamento', (req,res) => {
     res.render('../views/checkout-page');
 });
 
-app.get('/produto', (req,res) => {
+app.get('/produto-pagina', (req,res) => {
     res.render('../views/product-page');
 });
 
@@ -89,6 +70,14 @@ app.get('/cadastro-produto', (req,res) => {
 
 app.get('/cadastro-pedido', (req,res) => {
     res.render('../views/request-register');
+});
+
+app.get('/pedidos-pagina', (req,res) => {
+    res.render('../views/requests');
+});
+
+app.get('/clientes-pagina', (req,res) => {
+    res.render('../views/clients');
 });
 
 app.listen(port, function () {
